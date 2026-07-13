14deceb7c1e88da77390c1859921930c5aa3b86f
Icoo25 <h.stambolov@softart.bg>
2026-07-13 16:45:22 +0300

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false,
+  images: {
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 86400,
+  },
 };
 
 export default nextConfig;
